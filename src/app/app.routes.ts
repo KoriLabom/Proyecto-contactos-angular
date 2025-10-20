c7decd8d8a80a2fa518a42a4052b13049f0e39ef
Mateo Labombarda <ilovepll2005@gmail.com>
2025-10-20 09:57:37 -0300

    spinner al login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponente } from './pages/login-componente/login-componente';
-import { RegisterComponente } from './components/register-componente/register-componente';
+import { RegisterComponente } from './pages/register-componente/register-componente';
 import { LoggedLayout } from './logged-layout/logged-layout';
-import { ContactosComponente } from './components/contactos-componente/contactos-componente';
+import { ContactosComponente } from './pages/contactos-componente/contactos-componente';
 import { NewEditContact } from './pages/new-edit-contact/new-edit-contact';
 import { onlyUserGuard } from './guards/only-logged-user-guard';
 export const routes: Routes = [
--- src/app/pages/login-componente/login-componente.ts
+++ src/app/pages/login-componente/login-componente.ts
@@ -3,19 +3,28 @@ import { UserService } from '../../services/user-service';
 import { FormsModule } from '@angular/forms';
 import { LoginData } from '../../interfaces/auth';
 import { AuthService } from '../../services/auth-service';
+import { Spinner } from "../../components/spinner/spinner";
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-login-componente',
-  imports: [FormsModule],
+  imports: [FormsModule, Spinner, RouterModule],
   templateUrl: './login-componente.html',
   styleUrl: './login-componente.scss'
 })
 export class LoginComponente {
   auth=inject(AuthService)
-  login(form:any){
+  isLoading=false
+  errorLogin=false
+  async login(form:any){
+    console.log(form.value)
+    this.errorLogin = false;
     let logindata:LoginData={
       email:form.email,
       password:form.password
     }
-    this.auth.login(logindata)
+    this.isLoading = true;
+    await this.auth.login(logindata);
+    this.isLoading = false;
+    this.errorLogin = true;
   }
 }
